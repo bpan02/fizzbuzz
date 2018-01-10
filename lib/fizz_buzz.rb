adae717fbecf20883b937d969f1561ab01194010
Bonnie Pan <bpan@mdsol.com>
2018-01-10 15:13:11 -0500

    master: after interviw with Tom

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,31 @@
 
 class Fizzbuzz
-  def GetString(input)
-    if input % 3 == 0
-      return "fizz"
+  LookupTable =[
+    {
+      num:3,
+      name:"Fizz"},
+    {
+      num:5,
+      name:"Buzz"},
+    {
+      num:7,
+      name:"Bang"},
+  ].freeze
+
+  def get_string(input)
+    result = LookupTable.inject("") do |result, lookup|
+      # puts "lookup=" + lookup
+      if input % lookup[:num] == 0
+        result += lookup[:name]
+      else
+        result
+      end
     end
 
-    input.to_s
+    if result == ""
+      input.to_s
+    else
+      result
     end
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,40 +1,60 @@
 
 require 'fizz_buzz'
-
+#1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
 describe Fizzbuzz do
  describe ".GetString" do
    subject (:fizzbuzz) { Fizzbuzz.new }
 
    context "given a '1'" do
      it "returns 1" do
-       expect(fizzbuzz.GetString(1)).to eql("1")
+       expect(fizzbuzz.get_string(1)).to eql("1")
      end
    end
 
    context "given a '3'" do
-     it "returns 1" do
-       expect(fizzbuzz.GetString(3)).to eql("fizz")
+     it "returns Fizz" do
+       expect(fizzbuzz.get_string(3)).to eql("Fizz")
      end
    end
 
    context "given a '2'" do
-     it "returns 1" do
-       expect(fizzbuzz.GetString(2)).to eql("2")
+     it "returns 2" do
+       expect(fizzbuzz.get_string(2)).to eql("2")
      end
    end
 
    context "given a '6'" do
-     it "returns 1" do
-       expect(fizzbuzz.GetString(6)).to eql("fizz")
+     it "returns Fizz" do
+       expect(fizzbuzz.get_string(6)).to eql("Fizz")
      end
    end
 
    context "given a '5'" do
-     it "returns 1" do
-       expect(fizzbuzz.GetString(5)).to eql("buzz")
+     it "returns Buzz" do
+       expect(fizzbuzz.get_string(5)).to eql("Buzz")
+     end
+   end
+
+   context "given a '7'" do
+     it "returns Bang" do
+       expect(fizzbuzz.get_string(7)).to eql("Bang")
+     end
+   end
+   context "given a '15'" do
+     it "returns FizzBuzz" do
+       expect(fizzbuzz.get_string(15)).to eql("FizzBuzz")
+     end
+   end
+   context "given a '21'" do
+     it "returns FizzBang" do
+       expect(fizzbuzz.get_string(21)).to eql("FizzBang")
+     end
+   end
+   context "given a '105'" do
+     it "returns FizzBuzzBang" do
+       expect(fizzbuzz.get_string(105)).to eql("FizzBuzzBang")
      end
    end
 
  end
-
 end
