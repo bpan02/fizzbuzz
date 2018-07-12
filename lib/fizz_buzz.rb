ee908dd2517f86b5ca266ee3ab5cbb77af400e80
Bonnie Pan <bpan@mdsol.com>
2018-07-12 01:17:23 -0400

    master: adding more prep code for demo

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,17 @@
+
 class FizzBuzz
+  def prints(start_range)
+    end_range = start_range + 50
+    if (start_range < 1 || end_range < 1)
+      raise ArgumentError.new('start must be equal or greater than 1')
+    end
+    if (start_range > end_range)
+      raise ArgumentError.new('end_range must be equal or greater than start_range')
+    end
+
+    start_range.upto(end_range) {|i| puts get_string(i)}
+  end
+
   def get_string(input)
     result = ''
     dup = input.dup
@@ -28,3 +41,6 @@ class FizzBuzz
     end
   end
 end
+
+start = ARGV[0].to_i
+FizzBuzz.new.prints(start)
