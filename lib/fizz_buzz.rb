42cb5b5923b8b65a444bdcb22019e57c95bb667f
Bonnie Pan <bpan@mdsol.com>
2018-07-28 23:10:48 -0400

    master: adding ruby critic and set code sample to initial state

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,31 +1,26 @@
 class FizzBuzz
-  def get_string(input)
+  def fizz_buzzify(input)
     result = ''
-
     dup = input.dup
     while dup % 5 == 0 do
       result += 'Fizz'
       dup = dup/5
     end
-
+    input.to_s.each_char do |char|
+      if char.include?('5')
+        result += 'Fizz'
+      end
+    end
     dup = input.dup
     while dup % 7 == 0 do
       result += 'Buzz'
       dup = dup/7
     end
-
-    input.to_s.each_char do |char|
-      if char.include?('5')
-        result1 += 'Fizz'
-      end
-    end
-
     input.to_s.each_char do |char|
       if char.include?('7')
         result += 'Buzz'
       end
     end
-
     if result.empty?
       input.to_s
     else
@@ -33,11 +28,10 @@ class FizzBuzz
     end
   end
 
-
   def prints(range)
     result = ''
     range.each do |i|
-      result += get_string(i) + ' '
+      result += fizz_buzzify(i) + ' '
     end
     puts result
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,48 +1,48 @@
 require_relative '../lib/fizz_buzz'
 
 describe FizzBuzz do
-  describe '.get_string' do
+  describe '.fizz_buzzify' do
     subject (:fizzbuzz) { FizzBuzz.new }
 
     context "given a '1'" do
       it 'returns 1' do
-        expect(fizzbuzz.get_string(1)).to eql('1')
+        expect(fizzbuzz.fizz_buzzify(1)).to eql('1')
       end
     end
 
     context "given a '5'" do
       it 'returns Fizz' do
-        expect(fizzbuzz.get_string(5)).to eql('FizzFizz')
+        expect(fizzbuzz.fizz_buzzify(5)).to eql('FizzFizz')
       end
     end
 
     context "given a '2'" do
       it 'returns 2' do
-        expect(fizzbuzz.get_string(2)).to eql('2')
+        expect(fizzbuzz.fizz_buzzify(2)).to eql('2')
       end
     end
 
     context "given a '46'" do
       it 'returns 46' do
-        expect(fizzbuzz.get_string(46)).to eql('46')
+        expect(fizzbuzz.fizz_buzzify(46)).to eql('46')
       end
     end
 
     context "given a '47'" do
       it 'returns Buzz' do
-        expect(fizzbuzz.get_string(47)).to eql('Buzz')
+        expect(fizzbuzz.fizz_buzzify(47)).to eql('Buzz')
       end
     end
 
     context "given a '49'" do
       it 'returns BuzzBuzz' do
-        expect(fizzbuzz.get_string(49)).to eql('BuzzBuzz')
+        expect(fizzbuzz.fizz_buzzify(49)).to eql('BuzzBuzz')
       end
     end
 
     context "given a '50'" do
       it 'returns FizzFizzFizz' do
-        expect(fizzbuzz.get_string(50)).to eql('FizzFizzFizz')
+        expect(fizzbuzz.fizz_buzzify(50)).to eql('FizzFizzFizz')
       end
     end
   end
