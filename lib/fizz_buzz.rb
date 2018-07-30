300819fdd1e64307bd9430dcd9f348e7d191bdbe
Bonnie Pan <bpan@mdsol.com>
2018-07-29 21:50:58 -0400

    master: extract to a method, tests passing

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,31 +1,41 @@
 class FizzBuzz
   def fizz_buzzify(input)
     result = ''
-    dup = input.dup
-    while dup % 5 == 0 do
-      result += 'Fizz'
-      dup = dup/5
-    end
-    input.to_s.each_char do |char|
-      if char.include?('5')
-        result += 'Fizz'
-      end
-    end
+
+    result = divisible_by(input, 5, 'Fizz', result)
+
     dup = input.dup
     while dup % 7 == 0 do
       result += 'Buzz'
       dup = dup/7
     end
+
+    input.to_s.each_char do |char|
+      if char.include?('5')
+        result += 'Fizz'
+      end
+    end
+
     input.to_s.each_char do |char|
       if char.include?('7')
         result += 'Buzz'
       end
     end
+
     if result.empty?
       input.to_s
     else
       result
     end
+  end
+
+  def divisible_by(input, divisor, output, result)
+    dup = input.dup
+    while dup % divisor == 0 do
+      result += output
+      dup    = dup/5
+    end
+    result
   end
 
   def prints(range)
